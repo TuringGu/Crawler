69ed8a59bb85c35f413759b64e3f9355ecfa954c
Idea <gulintao850366208@gmail.com>
2020-04-21 11:16:51 +0800

    Solve type error of Session.send()
    You got me mad

--- Class_Exam/03_Requests/TODO_12_Prepared_Request.py
+++ Class_Exam/03_Requests/TODO_12_Prepared_Request.py
@@ -1,16 +1,16 @@
-# Have error: TypeError: a bytes-like object is required, not 'str'
 from requests import Request, Session
 
 url = 'http://httpbin.org/post'
-data = {'name', 'TuringGu'}
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
-}
+
+# Debug: 
+# 1. tranlate to byte to solve the problem
+# 2. Restart the environment
+# TypeError: a bytes-like object is required, not 'str'
+data = {'name': 'TuringGu'}   
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 s = Session()
 req = Request('POST', url, data=data, headers=headers)
-# prepped = req.prepare()
 prepped = s.prepare_request(req)
-r = s.send(prepped)
-# r = s.send(prepped.encode('utf-8'))
-print(r.text)
+resp = s.send(prepped)
+print(resp.text)
