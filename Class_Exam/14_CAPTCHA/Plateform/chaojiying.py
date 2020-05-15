f0b490b35392c9baa3b3407e6c561664cf50b139
Idea <gulintao850366208@gmail.com>
2020-05-15 19:27:14 +0800

    Steal need some optimize

--- Class_Exam/14_CAPTCHA/Plateform/chaojiying.py
+++ Class_Exam/14_CAPTCHA/Plateform/chaojiying.py
@@ -1,7 +1,8 @@
 import requests
+import sys
 from hashlib import md5
 
-class Chaojiying_Client(object):
+class Chaojiying(object):
 
     def __init__(self, username, password, soft_id):
         self.username = username
@@ -17,7 +18,7 @@ class Chaojiying_Client(object):
             'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
         }
 
-    def PostPic(self, im, codetype):
+    def post_pic(self, im, codetype):
         """
         im: 图片字节
         codetype: 题目类型 参考 http://www.chaojiying.com/price.html
@@ -30,7 +31,7 @@ class Chaojiying_Client(object):
         r = requests.post('http://upload.chaojiying.net/Upload/Processing.php', data=params, files=files, headers=self.headers)
         return r.json()
 
-    def ReportError(self, im_id):
+    def report_error(self, im_id):
         """
         im_id:报错题目的图片ID
         """
@@ -42,8 +43,9 @@ class Chaojiying_Client(object):
         return r.json()
 
 
+
 # if __name__ == '__main__':
-# 	chaojiying = Chaojiying_Client('超级鹰用户名', '超级鹰用户名的密码', '96001')   #用户中心>>软件ID 生成一个替换 96001
-# 	im = open('a.jpg', 'rb').read() #本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
-#     print(chaojiying.PostPic(im, 1902))  #1902 验证码类型  官方网站>>价格体系 3.4+版 print 后要加()
+#     chaojiying = Chaojiying('超级鹰用户名', '超级鹰用户名的密码', '96001')    #用户中心>>软件ID 生成一个替换 96001
+#     im = open('D:\\Develop\\Crawler\\Class_Exam\\14_CAPTCHA\\Plateform\\a.jpg', 'rb').read()  #本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
+#     print(chaojiying.post_pic(im, 1902))  #1902 验证码类型  官方网站>>价格体系 3.4+版 print 后要加()
 
